c00fc2db3240dbcb0dbfaf7e47905607946369c6
anshg03 <99557054+anshg03@users.noreply.github.com>
2025-03-02 23:17:59 +0530

    Basic Api Setup

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
+from .import views
 
-def hello_world(request):
-    return HttpResponse("Hello, world! This is our interneers-lab Django server.")
+# def hello_world(request):
+#     return HttpResponse("Hello, world!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_world),
+    path('hello/',views.hello_world,name='hello_world'),
 ]
---
+++ backend/django_app/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse,JsonResponse
+
+def hello_world(request):
+    """
+    A simple view that returns 'Hello, {name}' in JSON format.
+    Uses a query parameter named 'name'.
+    """
+    # Get 'name' from the query string, default to 'World' if missing
+    name = request.GET.get("name", "World")
+    return JsonResponse({"message": f"Hello, {name}!"})
