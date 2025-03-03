9248c1e3193e36f620a145fbb8a9e4ef7136d78b
anshg03 <99557054+anshg03@users.noreply.github.com>
2025-03-03 13:32:26 +0530

    Adding validation for different testing Scenarios

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from django.http import HttpResponse
 from .import views
 
-# def hello_world(request):
-#     return HttpResponse("Hello, world!")
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/',views.hello_world,name='hello_world'),
--- backend/django_app/views.py
+++ backend/django_app/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse,JsonResponse
+import re
+
+def error(message,status=400):
+    return JsonResponse({"error":message},status=status)
+
+def success(message,status=200):
+    return JsonResponse({"message":message},status=status)
+
 
 def hello_world(request):
-    """
-    A simple view that returns 'Hello, {name}' in JSON format.
-    Uses a query parameter named 'name'.
-    """
     # Get 'name' from the query string, default to 'World' if missing
     name = request.GET.get("name", "World")
-    return JsonResponse({"message": f"Hello, {name}!"})
+    #our name is only going to support letters with siz less than 10 and not empty
+    if len(name) >=10:
+        return error("Invalid name. Length must be less than 10.")
+    
+    if not re.match(r"^[A-Za-z]+$", name):
+        return error("Invalid name. Only letters are allowed.")
+
+    return success( f"Hello, {name}!")
